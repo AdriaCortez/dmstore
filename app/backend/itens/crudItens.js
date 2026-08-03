7bb499e751851a8419895f337c10fba0b94183c3
root <root@CFQ-NOTE-45.cfq.org.br>
2026-08-03 11:27:19 -0300

    crud de itens adicionado

---
+++ app/backend/itens/crudItens.js
@@ -0,0 +1,102 @@
+import { Item } from "../banco-de-dados/itens";
+import mongoose from "mongoose";
+
+export async function criarItem(req, res) {
+    try {
+        const { nome, preco, descricao, tipo } = req.body;
+
+        if(!nome || !preco || !descricao || !tipo) {
+            return res.status(400).json({
+                error: "Não foi possível criar o item. Todos os campos são obrigatórios!"
+            })}
+
+        const novoItem = await Item.create({
+            nome, 
+            descricao,
+            checklist,
+            tipo,
+            criadoPor: req.userId,
+            criadoEm: new Date(),
+        })
+
+        return res.status(201).json({
+            message: "Item criado com sucesso!",
+            item: novoItem
+        })
+    } catch (error) {
+        console.error("Erro ao criar item:", error);
+        return res.status(500).json({
+            error: "Erro ao criar item!"
+        });
+    }
+}
+
+export async function atualizarItem(req, res) {
+
+try {
+    const { itemId } = req.params;
+    const { nome, preco, descricao, tipo } = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(itemId)) {
+        return res.status(400).json({
+            error: "ID do item inválido!"
+        });
+    }
+
+    const item = await Item.findByIdAndUpdate(
+        itemId,
+        { nome, preco, descricao, tipo },
+        { new: true }
+    )
+
+    if(!item) {
+        return res.status(404).json({
+            error: "Item não encontrado!"
+        });
+    }
+
+    return res.status(200).json({
+        message: "Item atualizado com sucesso!",
+        item
+    });
+
+} catch (error) {
+    console.error("Erro ao atualizar item:", error);
+    return res.status(500).json({
+        error: "Erro ao atualizar item!"
+    });
+
+}}
+
+export async function deletarItem(req, res) {
+    try {
+
+        const { itemId } = req.params;
+
+        if(!mongoose.Types.ObjectId.isValid(itemId)) {
+            return res.status(400).json({
+                error: "ID do item inválido!"
+            });
+        }
+
+        const item = await Item.findById(itemId);
+
+        if(!item) {
+            return res.status(404).json({
+                error: "Item não encontrado!"
+            });
+        }
+
+        await Item.deleteOne({ _id: itemId });
+
+        return res.status(200).json({
+            message: "Item deletado com sucesso!"
+        });
+
+    } catch (error) {
+        console.error("Erro ao deletar item:", error);
+        return res.status(500).json({
+            error: "Erro ao deletar item!"
+        });
+    }
+}
